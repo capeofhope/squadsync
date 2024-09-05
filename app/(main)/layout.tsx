e078db345c4ea4b00301352d4eeb88d320432b5e
Mrinal <mrinalkandwal@gmail.com>
2024-09-06 04:34:05 +0530

    Error: Not s howing dropdownmenu

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,8 @@
 import {NavigationSidebar} from "@/components/navigation/navigation-sidebar";
-//error: the sidebar is not showing during adding to the functionalities on the add component
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+      <div className="invisible md:visible md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
         <NavigationSidebar/>
       </div>
       <main className="md:pl-[72px] h-full">{children}</main>
