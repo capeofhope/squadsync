5b7a9110fbfd0c57d22881fa57619867e5621b6f
Mrinal <mrinalkandwal@gmail.com>
2024-10-08 23:39:17 +0530

    Done with member page header with profile.

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
-import { Hash} from "lucide-react";
+import { Hash } from "lucide-react";
 import { MobileToggle } from "../mobile-toggle";
+import UserAvatar from "../user-avatar";
 
 interface ChatHeaderProps {
   serverTd: string;
@@ -10,9 +11,12 @@ interface ChatHeaderProps {
 const ChatHeader = ({ serverTd, name, type, imageUrl }: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <MobileToggle serverId={serverTd}/>
+      <MobileToggle serverId={serverTd} />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
